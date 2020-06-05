00bc7a40c19616711adeda224fa0321ee7fba625
Koen Vervloesem <koen@vervloesem.eu>
2020-06-05 16:08:55 +0200

    Add sphinx.ext.githubpages to docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ author = "Koen Vervloesem"
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.githubpages",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
