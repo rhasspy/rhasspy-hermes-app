207f41f4195079c11fa71f8f2ca1b98fa4f5d34e
Koen Vervloesem <koen@vervloesem.eu>
2020-06-09 08:59:24 +0200

    Fix test

--- tests/test_hotword.py
+++ tests/test_hotword.py
@@ -4,10 +4,11 @@ import asyncio
 
 import pytest
 from rhasspyhermes.wake import HotwordDetected
+
 from rhasspyhermes_app import HermesApp
 
 HOTWORD_TOPIC = "hermes/hotword/test/detected"
-HOTWORD_PAYLOAD = '{"modelId": "test_model.ppn", "modelVersion": "", "modelType": "personal", "currentSensitivity": 0.5, "siteId": "test_site", "sessionId": null, "sendAudioCaptured": null}'.encode()
+HOTWORD_PAYLOAD = '{"modelId": "test_model.ppn", "modelVersion": "", "modelType": "personal", "currentSensitivity": 0.5, "siteId": "test_site"}'
 
 _LOOP = asyncio.get_event_loop()
 
@@ -17,14 +18,19 @@ async def test_callbacks_hotword(mocker):
     """Test hotword callbacks."""
     app = HermesApp("Test HotwordDetected", mqtt_client=mocker.MagicMock())
 
+    # Mock wake callback and apply on_hotword decorator.
     wake = mocker.MagicMock()
-
     app.on_hotword(wake)
 
+    # Simulate app.run() without the MQTT client.
     app._subscribe_callbacks()
 
+    # Check whether callback has been added to the app.
     assert len(app._callbacks_hotword) == 1
+    assert app._callbacks_hotword[0] == wake
 
+    # Simulate detected hotword.
     await app.on_raw_message(HOTWORD_TOPIC, HOTWORD_PAYLOAD)
 
+    # Check whether callback has been called with the right Rhasspy Hermes object.
     wake.assert_called_once_with(HotwordDetected.from_json(HOTWORD_PAYLOAD))
