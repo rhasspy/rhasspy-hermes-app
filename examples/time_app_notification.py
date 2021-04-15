49c19fc478f6f9c94997de17decbd79f29a22c59
Koen Vervloesem <koen@vervloesem.eu>
2021-04-15 20:42:45 +0200

    make reformat

--- examples/time_app_notification.py
+++ examples/time_app_notification.py
@@ -1,13 +1,14 @@
 """Example app to tell you the time every minute with a notification."""
 import logging
-from datetime import datetime
 import threading
+from datetime import datetime
 
 from rhasspyhermes_app import HermesApp
 
 _LOGGER = logging.getLogger("TimeNotificationApp")
 SECONDS = 60
 SITE_ID = "default"
+
 
 def tell_time():
     """Tell the time with a notification."""
