ce86011fadd3ef4f8079b6efda7ea98feb6476c1
Koen Vervloesem <koen@vervloesem.eu>
2022-11-26 10:20:15 +0100

    Run CI on ubuntu-latest, add Python 3.9, 3.10

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Libraries",
     ],
     long_description=long_description,
